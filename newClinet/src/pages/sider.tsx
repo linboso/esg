05b6bdbd88f657887c061c1969bc64c1711d5c7b
CSL <johnsonafool@gmail.com>
2023-08-05 18:21:47 +0800

    sider

--- newClinet/src/pages/sider.tsx
+++ newClinet/src/pages/sider.tsx
@@ -1,8 +1,8 @@
 import logo from "./logo.svg"
-import { useCallback, useEffect, useState } from "react";
-import {Grid, Box, Card, Paper, Typography} from '@mui/material';
-import { useAppSelector, useAppDispatch } from '../app/hooks';
-import { NumericFormat } from 'react-number-format';
+import { useCallback, useEffect, useState } from "react"
+import { Grid, Box, Card, Paper, Typography } from "@mui/material"
+import { useAppSelector, useAppDispatch } from "../app/hooks"
+import { NumericFormat } from "react-number-format"
 import {
   Chart as ChartJS,
   RadialLinearScale,
@@ -14,10 +14,10 @@ import {
 } from "chart.js"
 
 import { Radar } from "react-chartjs-2"
-import { SumCarbonVolume } from "../slice/womSlice";
-import { useLocation } from "react-router-dom";
-import { useRequestProcessor } from "../hooks/useRequestProcessor";
-import { getData } from "../api/index";
+import { SumCarbonVolume } from "../slice/womSlice"
+import { useLocation } from "react-router-dom"
+import { useRequestProcessor } from "../hooks/useRequestProcessor"
+import { getData } from "../api/index"
 
 ChartJS.register(
   RadialLinearScale,
@@ -28,13 +28,14 @@ ChartJS.register(
   Legend,
 )
 
-
 function Sider() {
-  const [fontSize, setFontSize] = useState(200);
-  const [text, setText] = useState("20");
-  const weight: number[] = [5.25, 85.82, 18.08, 40.83, 173.53, 70, 16.8, 0, 54.67, 38.86];
-  const state = useAppSelector(state => state.wayOfMoving);
-  const dispatch = useAppDispatch();
+  const [fontSize, setFontSize] = useState(200)
+  const [text, setText] = useState("20")
+  const weight: number[] = [
+    5.25, 85.82, 18.08, 40.83, 173.53, 70, 16.8, 0, 54.67, 38.86,
+  ]
+  const state = useAppSelector((state) => state.wayOfMoving)
+  const dispatch = useAppDispatch()
   const { query } = useRequestProcessor()
 
   const {
@@ -45,63 +46,72 @@ function Sider() {
     enabled: true,
   })
 
-  const dot = (a: number[], b: number[]) => a.map((x, i) => a[i] * b[i]).reduce((m, n) => m + n).toFixed();  
+  const dot = (a: number[], b: number[]) =>
+    a
+      .map((x, i) => a[i] * b[i])
+      .reduce((m, n) => m + n)
+      .toFixed()
 
+  const avg = chartData ? dot((chartData as any).res2, weight) : null
 
-  const avg = chartData ? dot((chartData as any).res2, weight) : null;
-
-  const firstPage = useLocation().pathname  === "/" ? true : false;
-  const finalPage = useLocation().pathname  === "/page4" ? true : false;
-
+  const firstPage = useLocation().pathname === "/" ? true : false
+  const finalPage = useLocation().pathname === "/page4" ? true : false
 
   useEffect(() => {
-    dispatch(SumCarbonVolume());
-    updateFontSize(String(Math.floor(state.sum.CarbonVolume)));
+    dispatch(SumCarbonVolume())
+    updateFontSize(String(Math.floor(state.sum.CarbonVolume)))
     // updateFontSize("125");
     // setText("1230")
     // updateFontSize("10000");
-
-  },[state])
+  }, [state])
 
   const updateFontSize = useCallback(
-    (value:string) => {
-      if (value.length  == 5) {
-        setFontSize(68);
-
-      } else if (value.length  == 4) {
-        setFontSize(83);
+    (value: string) => {
+      if (value.length == 5) {
+        setFontSize(68)
+      } else if (value.length == 4) {
+        setFontSize(83)
       } else if (value.length == 3) {
-        setFontSize(115);
+        setFontSize(115)
       }
-      setText(value);
+      setText(value)
     },
-    [fontSize, text]
-  );
+    [fontSize, text],
+  )
+
+  console.log({ text })
 
   return (
     <>
-      <Box sx={{
-        // backgroundColor: "#15c924",
-        display: "flex",
-        // height: "100%",
-        padding: 7,
-        flexDirection: "column",
-      }}>
+      <Box
+        sx={{
+          // backgroundColor: "#15c924",
+          display: "flex",
+          // height: "100%",
+          padding: 7,
+          flexDirection: "column",
+        }}
+      >
         <Paper
-          elevation={4}  
+          elevation={4}
           sx={{
             // backgroundColor: "#254874",
             width: 540,
             height: 520,
             display: "flex",
             flexDirection: "column",
-        }}>
-          {finalPage? 
-            <Typography variant="h6" fontWeight="bold" mt={3} ml={3}>Comparison Emissions Kg-CO2</Typography>
-            :
-            <Typography variant="h6" fontWeight="bold" mt={3} ml={3}>Your Carbon Footprint</Typography>
-          }
-           <Box 
+          }}
+        >
+          {finalPage ? (
+            <Typography variant="h6" fontWeight="bold" mt={3} ml={3}>
+              Comparison Emissions Kg-CO2
+            </Typography>
+          ) : (
+            <Typography variant="h6" fontWeight="bold" mt={3} ml={3}>
+              Your Carbon Footprint
+            </Typography>
+          )}
+          <Box
             sx={{
               width: "80%",
               height: "80%",
@@ -109,73 +119,107 @@ function Sider() {
               alignSelf: "center",
               justifySelf: "center",
               display: "absolute",
-              margin: 5
-            }}>
-              {finalPage ?
+              margin: 5,
+            }}
+          >
+            {finalPage ? (
               <>
-                <Typography fontSize={fontSize} fontWeight="bold" 
+                <Typography
+                  fontSize={fontSize}
+                  fontWeight="bold"
                   sx={{
                     height: "20%",
                     marginLeft: "11%",
                     marginTop: "20%",
-                    color: "#474747",
+                    color: text !== "0" ? "#474747" : "#4C72D2",
+                    // color: "#4C72D2",
                     // backgroundColor: "#678ed6",
-                    maxWidth: 250
-                    }} >
-                  {/* {text}  */}
-                  <NumericFormat value={(Number(text)/1000).toFixed(1)} thousandSeparator="," displayType="text"/>
+                    maxWidth: 250,
+                  }}
+                >
+                  <NumericFormat
+                    value={
+                      text !== "0"
+                        ? (Number(text) / 1000).toFixed(1)
+                        : (Number(avg) / 1000).toFixed(1)
+                    }
+                    thousandSeparator=","
+                    displayType="text"
+                  />
                 </Typography>
-                <Typography variant="h3" fontWeight="bold" 
-                  sx={{
-                    // backgroundColor: "#ae21bb", 
-                    // color: "#474747",
-                    height: "20%",
-                    marginLeft: "60%", 
-                    marginTop: "-9%",
-                  }}>
+                {text !== "0" && (
+                  <Typography
+                    variant="h3"
+                    fontWeight="bold"
+                    sx={{
+                      // backgroundColor: "#ae21bb",
+                      // color: "#474747",
+                      height: "20%",
+                      marginLeft: "60%",
+                      marginTop: "-9%",
+                    }}
+                  >
                     /
-                </Typography>
-                <Typography variant="h3" fontWeight="bold" 
-                  sx={{
-                    // backgroundColor: "#d018e0", 
-                    color: "#4C72D2",
-                    height: "20%",
-                    marginLeft: "64%", 
-                    marginTop: "-18%",
-                  }}>
-                    <NumericFormat value={(Number(avg)/1000).toFixed(1)} thousandSeparator="," displayType="text"/> 
+                  </Typography>
+                )}
+                {text !== "0" && (
+                  <Typography
+                    variant="h3"
+                    fontWeight="bold"
+                    sx={{
+                      // backgroundColor: "#d018e0",
+                      color: "#4C72D2",
+                      height: "20%",
+                      marginLeft: "64%",
+                      marginTop: "-18%",
+                    }}
+                  >
+                    <NumericFormat
+                      value={(Number(avg) / 1000).toFixed(1)}
+                      thousandSeparator=","
+                      displayType="text"
+                    />
                     {/* 9999 */}
-                </Typography>
-                
-              </> 
-              :
-              firstPage? 
-                <img src="/hall.png" style={{
-                  alignSelf: "flex-end",
-                  height: "90%",
-                  padding: 20,
-                  marginTop: -20
-                }}/>  
-              
-              :
-              <>
-                <Typography fontSize={fontSize} fontWeight="bold" sx={{
-                    textJustify:"center", 
-                    textAlign:"center",
-                    // backgroundColor: "#c918da", 
-                    height: '100%',
-                    display:'flex',
-                    justifyContent:'center',
-                    alignItems:'center'
-                  }}>
-                  <NumericFormat value={(Number(text)/1000).toFixed(1)} thousandSeparator="," displayType="text" />
-                  {/* <NumericFormat value={Number(text)*1000} thousandSeparator="," displayType="text" /> */}
-                  <span style={{fontSize: 40, marginLeft: 3}}>Kg-CO2</span>
-                </Typography>
-                
+                  </Typography>
+                )}
               </>
-              }
-           </Box>
+            ) : firstPage ? (
+              <img
+                src="/hall.png"
+                style={{
+                  alignSelf: "flex-end",
+                  height: "80%",
+                  width: "80%",
+                  padding: 20,
+                  marginTop: -20,
+                }}
+              />
+            ) : (
+              <>
+                <Typography
+                  fontSize={fontSize}
+                  fontWeight="bold"
+                  sx={{
+                    textJustify: "center",
+                    textAlign: "center",
+                    // backgroundColor: "#c918da",
+                    height: "100%",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  <NumericFormat
+                    value={(Number(text) / 1000).toFixed(1)}
+                    thousandSeparator=","
+                    displayType="text"
+                  />
+                  {/* <NumericFormat value={Number(text)*1000} thousandSeparator="," displayType="text" /> */}
+                  <span style={{ fontSize: 40, marginLeft: 3 }}>Kg-CO2</span>
+                </Typography>
+              </>
+            )}
+          </Box>
         </Paper>
       </Box>
     </>
@@ -183,4 +227,3 @@ function Sider() {
 }
 
 export default Sider
-
